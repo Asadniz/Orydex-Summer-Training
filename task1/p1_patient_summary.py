9feed2c6aceb491ce8a6fb2ad51048c5f775aba0
Asadniz <156411879+Asadniz@users.noreply.github.com>
2026-06-28 12:20:13 +0500

    Add patient summary and collection operations

--- task1/p1_patient_summary.py
+++ task1/p1_patient_summary.py
@@ -12,37 +12,76 @@ patients = [
     {"id": 4, "name": "Bilal Malik", "age": 52, "condition": "diabetes", "active": True},
 ]
 
+from helper import b_sort
+
 
 def total_patients(patient_records):
     """Return the total number of patients."""
     # TODO: Implement this function.
-    pass
+    count = 0
+    for patient in patient_records:
+        count += 1
+    return count
 
 
 def average_age(patient_records):
     """Return the average patient age."""
     # TODO: Implement this function.
-    pass
+    age_sum = 0
+    for patient in patient_records:
+        age_sum += patient["age"]
+    return (age_sum/total_patients(patient_records))
 
 
 def count_active_patients(patient_records):
     """Return the number of active patients."""
     # TODO: Implement this function.
-    pass
+    count = 0
+    for patient in patient_records:
+        if patient["active"]:
+            count += 1
+    return count
 
 
 def unique_conditions(patient_records):
     """Return a sorted list of unique conditions."""
     # TODO: Implement this function.
-    pass
+    lst = []
+    for patient in patient_records:
+        if patient["condition"] not in lst:
+            lst.append(patient["condition"])
+
+    b_sort(lst)
+
+    return lst
 
 
 def count_by_condition(patient_records):
     """Return a dictionary containing patient count by condition."""
     # TODO: Implement this function.
-    pass
+    patients_by_condition = {}
+    for patient in patient_records:
+        count = 1
+        for temp in patient_records:
+            if patient["id"] != temp["id"] and patient["condition"] == temp["condition"]:
+                count += 1
+        patients_by_condition[patient["condition"]] = count
+    return patients_by_condition
+
 
 
 if __name__ == "__main__":
     # TODO: Print the summary results clearly.
-    pass
+    print ("SUMMARY")
+    print ("----------")
+    print ("Patients: ")
+    
+    for patient in patients:
+        print (patient)
+
+    print ("Total patients: ", total_patients(patients))
+    print ("Average Age: ", average_age(patients))
+    print ("Active Patients: ", count_active_patients(patients))
+    print ("Unique Conditions: ", unique_conditions(patients))
+    print ("Patients by Conditions: ", count_by_condition(patients))
+    print ("----------")
