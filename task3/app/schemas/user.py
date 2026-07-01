bb8bfed007f1b738aa3d44ccaa6fcfd2953a42cb
Asadniz <156411879+Asadniz@users.noreply.github.com>
2026-07-02 02:09:17 +0500

    feat: add user schemas (Create, Read, Login, Token)

---
+++ task3/app/schemas/user.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field
+
+class UserCreate(BaseModel):
+    username: str = Field(..., min_length=1, max_length=100)
+    password: str = Field(..., min_length=8)
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class UserRead(BaseModel):
+    id: int
+    username: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
