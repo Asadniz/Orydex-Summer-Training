bc456739a1e251844f9894fa40730cb6f36dbf7d
Asadniz <156411879+Asadniz@users.noreply.github.com>
2026-07-02 19:28:09 +0500

    feat: add OpenAPI descriptions, schema examples, and app metadata

--- task3/app/schemas/patient.py
+++ task3/app/schemas/patient.py
@@ -1,19 +1,45 @@
 from pydantic import BaseModel, Field
 from typing import Optional
 
+
 class PatientCreate(BaseModel):
     name: str = Field(..., min_length=1, max_length=100)
     age: int = Field(..., ge=0, le=120)
     condition: str = Field(..., min_length=1)
     risk_score: int = Field(..., ge=0, le=100)
     active: bool = Field(default=True)
+    model_config = {
+        "json_schema_extra":{
+            "example":{
+                "name": "Asadullah Nizamani",
+                "age": 20,
+                "condition": "Lymphoma",
+                "risk_score": 90,
+                "active": True
+            }
+        }
+    }
+    
+
 
 class PatientPatch(BaseModel):
-    name: Optional[str] = Field(None, min_length=1, max_length=100)
+    name: Optional[str] = Field(
+        None, min_length=1, max_length=100)
     age: Optional[int] = Field(None, ge=0, le=120)
     condition: Optional[str] = Field(None, min_length=1)
     risk_score: Optional[int] = Field(None, ge=0, le=100)
     active: Optional[bool]
+    model_config = {
+        "json_schema_extra":{
+            "example":{
+                "name": "Asadullah Nizamani",
+                "age": 20,
+                "condition": "Lymphoma",
+                "risk_score": 90,
+                "active": True
+            }
+        }
+    }
 
 class PatientRead(BaseModel):
     id: int
@@ -22,5 +48,16 @@ class PatientRead(BaseModel):
     condition: str
     risk_score: int
     active: bool
-    
-    model_config = {"from_attributes": True}
+
+    model_config = {
+        "from_attributes": True,
+        "json_schema_extra":{
+            "example":{
+                "name": "Asadullah Nizamani",
+                "age": 20,
+                "condition": "Lymphoma",
+                "risk_score": 90,
+                "active": True
+            }
+        }
+    }
