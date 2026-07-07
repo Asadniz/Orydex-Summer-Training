a44a5371ba4119ae80c122e0b499e8ff48e02c3f
Asadniz <156411879+Asadniz@users.noreply.github.com>
2026-07-07 13:54:26 +0500

    ruff reformatting

--- task1/p7_runner_up.py
+++ task1/p7_runner_up.py
@@ -15,7 +15,7 @@ def find_runner_up(scores: list[int]) -> int:
     Example: [2, 3, 6, 6, 5] -> 5 (6 is the highest, 5 is the runner-up).
     """
     # TODO: Remove duplicate scores, then return the second largest value.
-    
+
     if len(scores) <= 1:
         return -1
 
@@ -23,17 +23,17 @@ def find_runner_up(scores: list[int]) -> int:
     for num in scores:
         if num not in lst:
             lst.append(num)
-    
+
     if len(lst) == 1:
         return -1
-    
+
     if lst[0] > lst[1]:
         best = lst[0]
         runner_up = lst[1]
     else:
         best = lst[1]
         runner_up = lst[0]
-    
+
     for i in range(2, len(lst)):
         if lst[i] > best:
             runner_up = best
